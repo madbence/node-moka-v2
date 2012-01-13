a118dd9ac589f3abd6e701cccf2afc435d61b409
Bence Danyi <madbence@gmail.com>
2012-01-13 15:26:53 +0100

    BNC cleaned up

--- bnc.js
+++ bnc.js
@@ -1,22 +1,14 @@
 var server=process.argv[2]||'atw.irc.hu';
 var port=process.argv[3]||6669;
-var listen=process.argv[4]||1337;
-
 var net = require('net');
-
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdout.setEncoding('utf8');
-
 process.stdin.on('data', function(data)
 {
 	f.write(data);
 });
-
-var f=net.connect(port, server, function(succ)
-{
-	console.log('BNC connected');
-});
+var f=net.connect(port, server);
 f.on('data', function(data)
 {
 	process.stdout.write(data.toString());
