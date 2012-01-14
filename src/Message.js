0aada0cbd83501428ba7135ea823ac4ee69884f3
Bence Danyi <madbence@gmail.com>
2012-01-14 22:00:20 +0100

    Message.js bugfix/typo

--- src/Message.js
+++ src/Message.js
@@ -3,11 +3,13 @@ var Message=function(string)
 	var match=string.match(/^(:([^ ]+) |)([0-9]{3}|[A-Z]+) ([^:]*):(.*?)$/);
 	if(!match)
 	{
-		throw new Error('\''+string.substr(0, 20)+'\' is a malformed message (?)');
+		throw new Error('\''+string.substr(0, 10)+'...\' is a malformed message (its beginnig/end is maybe in the prevorius/next buffer?)');
 	}
 	this.origin=match[2];
 	this.response=match[3];
 	this.trail=match[5];
+	var parameters=match[4].split(' ');
+	this.parameters=parameters.slice(0, parameters.length-1);
 	this.raw=string;
 }
 
@@ -37,13 +39,17 @@ Message.prototype=
 	{
 		return parseInt(string.replace(/^0*/, ''));
 	},
-	'getTrail': function()
+	'getTail': function()
 	{
 		return this.trail;
 	},
 	'getRaw': function()
 	{
 		return this.raw;
+	},
+	'getParameters': function()
+	{
+		return this.parameters;
 	},
 	'getNumericResponseType': function()
 	{
