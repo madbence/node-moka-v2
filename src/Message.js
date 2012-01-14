7077633864cf57d87ab7e74de4d4202076ab0223
Bence Danyi <madbence@gmail.com>
2012-01-14 18:44:51 +0100

    Message.js updated

--- src/Message.js
+++ src/Message.js
@@ -1,17 +1,18 @@
 var Message=function(string)
 {
-	var match=string.match(/^(:([^ ]+) |)([0-9]{3}|[A-Z]+) (\S+)/);
-	//console.log(match);
+	var match=string.match(/^(:([^ ]+) |)([0-9]{3}|[A-Z]+) ([^:]*):(.*?)$/);
 	if(!match)
 	{
-		throw new Error('\''+string+'\' is a malformed message (?)');
+		throw new Error('\''+string.substr(0, 20)+'\' is a malformed message (?)');
 	}
 	this.origin=match[2];
 	this.response=match[3];
+	this.trail=match[5];
+	this.raw=string;
 }
 
 Message.prototype=
-{
+{	
 	'getOrigin': function()
 	{
 		return this.origin;
@@ -35,7 +36,29 @@ Message.prototype=
 	'parseNumericResponse': function(string)
 	{
 		return parseInt(string.replace(/^0*/, ''));
-	}
+	},
+	'getTrail': function()
+	{
+		return this.trail;
+	},
+	'getRaw': function()
+	{
+		return this.raw;
+	},
+	'getNumericResponseType': function()
+	{
+		return MessageCodes[this.getNumericResponse()]?MessageCodes[this.getNumericResponse()]:0;
+	},
 }
+
+var MessageCodes=
+{
+	1: 'RPL_WELCOME',
+	375: 'RPL_MOTDSTART',
+	376: 'RPL_ENDOFMOTD',
+	372: 'RPL_MOTD',
+}
+	
+	
 
 exports.Message=Message;
