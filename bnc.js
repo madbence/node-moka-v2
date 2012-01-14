dbef2aae4730a68d85cf830858833be8bbad6f33
Bence Danyi <madbence@gmail.com>
2012-01-14 16:10:15 +0100

    BNC refactored

--- bnc.js
+++ bnc.js
@@ -1,15 +1,74 @@
 var server=process.argv[2]||'atw.irc.hu';
 var port=process.argv[3]||6669;
 var net = require('net');
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdout.setEncoding('utf8');
-process.stdin.on('data', function(data)
+
+var bnc=
 {
-	f.write(data);
-});
-var f=net.connect(port, server);
-f.on('data', function(data)
+	'connection': null,
+	'connect':function(server, port)
+	{
+		if(this.hasActiveConnection())
+		{
+			return;
+		}
+		this.makeConnection(server||'atw.irc.hu', port||6669);
+	},
+	'makeConnection': function(server, port)
+	{
+		this.connection=net.connect(port, server, this.onConnect);
+		this.connection.on('data', this.dataHandler);
+		this.connection.on('close', this.onDisconnect);
+	},
+	'onConnect': function(){},
+	'onDisconnect': function(){},
+	'dataHandler': function(){},
+	'disconnect':function(hard)
+	{
+		if(!this.hasActiveConnection())
+		{
+			return;
+		}
+		if(hard)
+		{
+			this.connection.destroy();
+		}
+		else
+		{
+			this.connection.destroySoon();
+		}
+	},
+	'hasActiveConnection': function()
+	{
+		return this.connection != null &&
+			this.connection.readable &&
+			this.connection.writeable;
+	}
+};
+
+var identServer=
 {
-	process.stdout.write(data.toString());
-});
+	'start': function(name, port, dataclb, closeclb)
+	{
+		var server=identID=net.createServer(function (socket) {
+			socket.on('data', function(data)
+			{
+				socket.write(data.toString().replace('\r\n', '')+' : USERID : '+name+'\r\n');
+				if(dataclb && typeof dataclb === 'function')
+				{
+					dataclb();
+				}
+			});
+			socket.on('close', function()
+			{
+				server.close();
+				if(closeclb && typeof closeclb === 'function')
+				{
+					closeclb();
+				}
+			});
+		}).listen(port||113);
+	}
+}
+
+exports.bnc=bnc;
+exports.identServer=identServer;
